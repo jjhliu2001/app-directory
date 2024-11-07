cb8186c6243f1e5374dee07b0d7ff2e6e93a081f
oliverchanyw <oliverchanyw@gmail.com>
2024-11-07 08:02:15 +0800

    chore: todo jen meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Byline from '#/ui/byline';
 import { GlobalNav } from '#/ui/global-nav';
 import { Metadata } from 'next';
 
+// TODO - Jen
 export const metadata: Metadata = {
   title: {
     default: 'Next.js App Router',
@@ -31,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="[color-scheme:dark]">
       <body className="overflow-y-scroll bg-gray-1100 bg-[url('/grid.svg')] pb-36">
-        <GlobalNav />
+        {/* <GlobalNav />
 
         <div className="lg:pl-72">
           <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
@@ -39,14 +40,14 @@ export default function RootLayout({
               <div className="rounded-lg bg-black">
                 <AddressBar />
               </div>
-            </div>
+            </div> */}
 
-            <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
-              <div className="rounded-lg bg-black p-3.5 lg:p-6">{children}</div>
-            </div>
-            <Byline />
-          </div>
+        <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
+          <div className="rounded-lg bg-black p-3.5 lg:p-6">{children}</div>
         </div>
+        {/* <Byline /> */}
+        {/* </div>
+        </div>  */}
       </body>
     </html>
   );
